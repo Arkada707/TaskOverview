6cde97cc10c98a180375f327717c7f9c14c77ff4
Arkada <73377469+Arkada707@users.noreply.github.com>
2023-07-10 08:57:43 +0000

    Updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { copyFileSync } from 'fs';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
     outDir: 'dist',
-    afterBuild: () => {
-      const sourcePath = resolve(__dirname, 'ads.txt');
-      const destinationPath = resolve(__dirname, 'dist/ads.txt');
-      copyFileSync(sourcePath, destinationPath);
-    },
+    assetsDir: '.', // Include assets from the root directory
   },
 });
